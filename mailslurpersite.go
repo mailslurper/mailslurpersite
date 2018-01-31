d36d1303667adc1aec81ba01af0dd9714bbedd94
Adam Presley <adampresley@Adams-MacBook-Pro.local>
2018-01-31 15:02:55 -0600

    Embedding assets

--- mailslurpersite.go
+++ mailslurpersite.go
@@ -1,3 +1,5 @@
+//go:generate esc -o ./assets/www.go -pkg assets -ignore DS_Store|README\.md|LICENSE|www\.go -prefix /assets/ ./assets ./index.html
+
 package main
 
 import (
@@ -5,14 +7,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/mailslurper/mailslurpersite/assets"
 )
+
+const DEBUG_ASSETS bool = false
 
 var port = flag.Int("port", 8889, "Port to bind this server to")
 
 func main() {
 	flag.Parse()
 
-	fileServer := http.FileServer(http.Dir("./"))
+	fileServer := http.FileServer(assets.FS(DEBUG_ASSETS))
 	http.Handle("/", fileServer)
 
 	log.Println("Serving content for MailSlurperSite on port", *port)
